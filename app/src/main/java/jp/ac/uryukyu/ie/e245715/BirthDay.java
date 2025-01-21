c8610b34be9acef8ebef1e2d51c7add25021858d
chikako ooshiro <e245715@ie.u-ryukyu.ac.jp>
2025-01-21 13:38:24 +0900

    BirthDay.java 1st_commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e245715/BirthDay.java
@@ -0,0 +1,26 @@
+package jp.ac.uryukyu.ie.e245715;
+
+import java.time.LocalDate;
+
+public class BirthDay extends Person{
+
+    BirthDay(String name,LocalDate birthDate){
+        super(name, birthDate);
+    }
+
+    //誕生日かどうかを調べる
+    public void checkBirthDay(){
+
+        LocalDate today = LocalDate.now();
+
+        if(today == getBirthDay()){
+            System.out.println(getName() + "さん HappyBirthDay!! いい一年になりますように！");
+        }else{
+            System.out.println("誕生日までのお楽しみに！");
+        }
+    }
+
+
+
+    
+}
