12d9ee3215776e9bc2f556b7afb7c949575295ac
chikako ooshiro <e245715@ie.u-ryukyu.ac.jp>
2025-01-22 01:45:00 +0900

    FcMember.java 4th_commit

--- app/src/main/java/jp/ac/uryukyu/ie/e245715/FcMember.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245715/FcMember.java
@@ -26,13 +26,13 @@ public class FcMember extends Person {
         this.Oshimen = Oshimen;
     }
 
-   /*コンストラクタ
+   //コンストラクタ
     public FcMember(String name,LocalDate birthDate,LocalDate joinDate,String Oshimen){
 
         super(name, birthDate);
         this.joinDate = joinDate;
         this.Oshimen = Oshimen;
-    } */
+    } 
 
     
 
@@ -48,11 +48,13 @@ public class FcMember extends Person {
     
     //マイプロフィールを表示するメソッド
     public void displayMyProfile(){
-        System.out.println( "名前："+ getName() + 
-                            "誕生日：" + getBirthDay() + 
-                            "入会日：" + getJoinDate() + 
-                            "会員歴：" + daysBetween + "日" +
-                            "推しメンバー" + getOshimen());
+
+        long daysBetween = ChronoUnit.DAYS.between(joinDate,LocalDate.now());
+        System.out.println( "(名前)"+ getName() + 
+                            "(誕生日)" + getBirthDay() + 
+                            "(入会日)" + getJoinDate() + 
+                            "(会員歴)" + daysBetween + "日" +
+                            "(推しメンバー)" + getOshimen());
     }
 
     
